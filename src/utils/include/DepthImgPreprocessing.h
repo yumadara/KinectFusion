033c3b76f53f02c30a943942dade89fb52e39dff
yumadara <joechiyu@gmail.com>
2021-12-20 00:44:28 +0100

    DepthImagePreprocessing tested

--- src/utils/include/DepthImgPreprocessing.h
+++ src/utils/include/DepthImgPreprocessing.h
@@ -1,14 +1,40 @@
 #include <iostream>
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-
+#include <math.h>
+#include "Eigen.h"
 using namespace std;
-using namespace cv;
 
-
-void depthMapPreprocessing(float* source, float* result )
+float calculatePreprocessedValue(int p, int q, Matrix2f& source, float sigma_s, float sigma_r)
 {
-	bilateralFilter(source, result, 4, 2, 2);
+	float result=0;
+	float factor=0;
+	int rows = source.rows();
+	int cols = source.cols();
+	for (int i = 0; i != rows; i++)
+	{
+		for (int j = 0; j != cols; j++)
+		{
+			float pixelPosNormSquare = (pow(i - p, 2) + pow(j - q, 2));
+			float pixelValueNormSquare = (pow(source(p,q) - source(i,j), 2));
+			result += exp(-pixelPosNormSquare / pow(sigma_s, 2)) * exp(-pixelValueNormSquare / pow(sigma_r, 2)) * source(i,j);
+			
+			factor += exp(-pixelPosNormSquare / pow(sigma_s, 2)) * exp(-pixelValueNormSquare / pow(sigma_r, 2));
+			
+		}
+	}
+	
+	return result / factor;
+}
+
+void BilateralFilter(Matrix2f& source, Matrix2f& destination, float sigma_s, float sigma_r)
+{
+	int rows = source.rows();
+	int cols = source.cols();
+	for (int i = 0; i != rows; i++)
+	{
+		for (int j = 0; j != cols; j++)
+		{
+			destination(i,j) = calculatePreprocessedValue(i,j, source, sigma_s, sigma_r);
+		}
+	}
 }
 
---
+++ test/DepthImgPreprocessingTest.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "../src/utils/include/DepthImgPreprocessing.h"
+// Demonstrate some basic assertions.
+TEST(DepthImgPreprocessingTest, BasicAssertions) {
+	// Expect two strings not to be equal.
+	Matrix2f source;
+	Matrix2f destination;
+	source(0,0) = 1;
+	source(0,1) = 0.5;
+	source(1,0) = 0.2;
+	source(1,1) = 1;
+	BilateralFilter(source, destination, 1, 1);
+	cout << destination(0,0);
+	EXPECT_TRUE(destination(0,0)<0.816 && destination(0,0)>0.815);
+}
