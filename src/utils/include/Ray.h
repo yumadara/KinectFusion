7d359eb4e0a3d31160d82e43d2bb2ca0bef85151
yamei <1842850321@qq.com>
2021-12-23 17:13:43 +0100

    new file:   src/utils/include/Ray.h

---
+++ src/utils/include/Ray.h
@@ -0,0 +1,47 @@
+#include "Eigen.h"
+
+
+class Ray {
+    public:
+    Ray(float stepLength, float minDistance, float maxDistance, Vector3f direction){
+        this->stepLength = stepLength;
+        this->minDistance = minDistance;
+        this->maxDistance = maxDistance;
+        this->direction = direction / direction.norm();
+        this->currLocation = this->direction * minDistance;
+        this->lastLocation = this->currLocation;
+        
+
+        
+    }
+    ~Ray(){}
+
+    void step(){
+        this->lastLocation = this->currLocation;
+        this->currLocation = this->currLocation + this->direction * this->stepLength;
+
+
+
+    }
+    Vector3f getCurrLocation(){
+        return this->currLocation;
+    }
+    Vector3f getLastLocation(){
+        return this->lastLocation;
+    }
+    bool isInBound(){
+        return this->currLocation.norm()<=this->maxDistance;
+    }
+
+    private:
+    float stepLength;
+    float minDistance;
+    float maxDistance;
+    Vector3f direction;
+    Vector3f currLocation;
+    Vector3f lastLocation;
+    
+
+
+
+};
