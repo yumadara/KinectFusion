e8df95e20759c3de156e182b00c48a972bc37f56
Ruslan Mammadov <ruslan.mammadov@tum.de>
2022-01-03 17:45:00 +0400

    Performed refactoring of surface_measurement
    * Update CMakeLists.txt
    * Move static functions to surface_measurements_utils.h
    * Add doxygen to type definitions
    * Move Level to type definitions
    
    * Updated

--- src/surface_measurement/src/data_frame.cpp
+++ src/surface_measurement/src/data_frame.cpp
@@ -1,144 +1,37 @@
 #include <data_frame.h>
 
+#include <surface_measurement_utils.h>
+
 namespace kinect_fusion {
-    /**
-     * @brief Max distance, or 3 sigmas. For subsampling, when averaging the block of pixels,
-     * if value of neighbor pixel is MAX_DISTANCE greater/smaller than the value of center pixel,
-     * it is not used for averaging. 
-     */
-    constexpr float MAX_DISTANCE = 0.1f;
 
-    FrameData::FrameData(const Eigen::Matrix3f& cameraIntrinstics, std::size_t height, std::size_t width)
-        {
-            for (Level level : LEVELS) {
-                std::size_t index = getIndex(level);
-                std::size_t scale = std::pow(2U, index);
-
-                std::size_t current_height = height / scale;
-                std::size_t current_width = width / scale;
-
-                m_cameraIntrinstics[index] = computeLevelCameraIntrinstics(cameraIntrinstics, level);
-
-                m_normalMaps[index] = Map2DVector3f(current_height, current_width);
-                m_vertexMaps[index] = Map2DVector3f(current_height, current_width);
-                m_filteredDepthMaps[index] = Map2Df(current_height, current_width);
-            } 
-        }
-
-        void FrameData::updateValues(const Map2Df&  depths) {
-            m_rowDepthMap = depths;
-
-            m_filteredDepthMaps[0] = depths;
-            for (std::size_t i = 0; i < NUMBER_OF_LEVELS - 1; i++) {
-                subsample(m_filteredDepthMaps[i], m_filteredDepthMaps[i + 1]);
-            }
-            for (std::size_t i = 0; i < NUMBER_OF_LEVELS; i++) {
-                fillVertexMap(m_filteredDepthMaps[i], m_cameraIntrinstics[i], m_vertexMaps[i]);
-                fillNormalMap(m_vertexMaps[i], m_normalMaps[i]);
-            }
-        }
-
-        void FrameData::fillVertexMap(const Map2Df& depths, const Eigen::Matrix3f& depthIntrinsics, Map2DVector3f& vertexMap) {
-            // Get depth intrinsics.
-            float fovX = depthIntrinsics(0, 0);
-            float fovY = depthIntrinsics(1, 1);
-            float cX = depthIntrinsics(0, 2);
-            float cY = depthIntrinsics(1, 2);
-
-            // For every pixel row.
-    #pragma omp parallel for
-            for (int row = 0; row < depths.height(); ++row) {
-                // For every pixel in a row.
-                for (int col = 0; col < depths.width(); ++col) {
-                    std::size_t idx = depths.getIndex(row, col); // linearized index
-                    float depth = depths.get(idx);
-                    if (depth == MINF) {
-                        vertexMap.get(idx) = Vector3f(MINF, MINF, MINF);
-                    }
-                    else {
-                        // Back-projection to camera space.
-                        vertexMap.get(idx) = Vector3f((col - cX) / fovX * depth, (row - cY) / fovY * depth, depth);
-                    }
-                }
-            }
-        }
-
-        void FrameData::fillNormalMap(const Map2DVector3f& vertexMap, Map2DVector3f& normalMap) {
-
- #pragma omp parallel for
-            for (std::size_t row = 1; row < vertexMap.height() - 1; ++row) {
-                for (std::size_t col = 1; col < vertexMap.width() - 1; ++col) {
-                    std::size_t idx = vertexMap.getIndex(row, col); // linearized index
-
-                    auto firstVector{vertexMap.get(idx + vertexMap.width()) - vertexMap.get(idx)};
-                    auto secondVector{vertexMap.get(idx + 1) - vertexMap.get(idx)};
-
-                    if (!firstVector.allFinite() || !secondVector.allFinite()) {
-                        normalMap.get(idx) = Vector3f(MINF, MINF, MINF);
-                    } else {
-                        normalMap.get(idx) = firstVector.cross(secondVector);
-                        normalMap.get(idx).normalize();
-                    }       
-                }
-            }
-        }
-
-        void FrameData::subsample(const Map2Df& previousDepthMap, Map2Df& nextDepthMap)
-        {
-            for (int row = 0; row < previousDepthMap.height() - 1; row += 2) {
-                for (int col = 0; col < previousDepthMap.width() - 1; col += 2) {
-                    int topNeighbourRow = row - 1;
-                    int leftNeighbourCol = col - 1;
-                    int numberOfPixels = 9;
-
-                    if (col == 0) {
-                        leftNeighbourCol = 0;
-                        numberOfPixels = 6;
-                    }
-                    if (row == 0){
-                        topNeighbourRow = 0;
-                        numberOfPixels = 6;
-                    }
-                    if (col == 0 && row == 0)
-                    {
-                        numberOfPixels = 4;
-                    }
-
-                    float centerPixel = previousDepthMap.get(row, col);
-                    float& newPixel = nextDepthMap.get(row / 2, col / 2);
-                    newPixel = 0;
-
-                    for (int neighbourRow = topNeighbourRow; neighbourRow <= row + 1; neighbourRow++) {
-                        for (int neighbourCol = leftNeighbourCol; neighbourCol <= col + 1; neighbourCol++) {
-                            float neighbourValue = previousDepthMap.get(neighbourRow, neighbourCol);
-                            if (std::abs(neighbourValue - centerPixel) > MAX_DISTANCE) {
-                                numberOfPixels--;
-                            } else {
-                                newPixel += neighbourValue;
-                            }
-                            newPixel /= numberOfPixels;
-                        }                 
-                    }
-                }
-            }
-
-        }
-
-    Eigen::Matrix3f FrameData::computeLevelCameraIntrinstics(const Eigen::Matrix3f& originalCameraIntrinstics, Level level)
+FrameData::FrameData(const Eigen::Matrix3f& cameraIntrinstics, std::size_t height, std::size_t width)
     {
-        Eigen::Matrix3f levelCameraIntrinstics{originalCameraIntrinstics};
+        for (Level level : LEVELS) {
+            std::size_t index = getIndex(level);
+            std::size_t scale = std::pow(2U, index);
 
-        if (level == Level::First) {
-            return levelCameraIntrinstics;
-        }
+            std::size_t current_height = height / scale;
+            std::size_t current_width = width / scale;
 
-        float scale = std::pow(0.5f, static_cast<std::size_t>(level));
+            m_cameraIntrinstics[index] = computeLevelCameraIntrinstics(cameraIntrinstics, level);
 
-        levelCameraIntrinstics(0, 0) *= scale; // focal_x
-        levelCameraIntrinstics(1, 1) *= scale; // focal y
-        levelCameraIntrinstics(0, 1) *= scale; // skew coefficient
-        // Since first pixel is as it was at (0, 0), we do not need to change principal point!
-
-        return levelCameraIntrinstics;
+            m_normalMaps[index] = Map2DVector3f(current_height, current_width);
+            m_vertexMaps[index] = Map2DVector3f(current_height, current_width);
+            m_filteredDepthMaps[index] = Map2Df(current_height, current_width);
+        } 
     }
+
+    void FrameData::updateValues(const Map2Df&  depths) {
+        m_rowDepthMap = depths;
+
+        m_filteredDepthMaps[0] = depths;
+        for (std::size_t i = 0; i < NUMBER_OF_LEVELS - 1; i++) {
+            subsample(m_filteredDepthMaps[i], m_filteredDepthMaps[i + 1]);
+        }
+        for (std::size_t i = 0; i < NUMBER_OF_LEVELS; i++) {
+            fillVertexMap(m_filteredDepthMaps[i], m_cameraIntrinstics[i], m_vertexMaps[i]);
+            fillNormalMap(m_vertexMaps[i], m_normalMaps[i]);
+        }
+    }
+
 } // namespace kinect_fusion
