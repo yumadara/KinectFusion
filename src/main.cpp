10326af7ac40078bdb2710a0d46979d65d4a39aa
Ruslan Mammadov <ruslan.mammadov@tum.de>
2022-01-12 21:31:12 +0100

    Write first part for main

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,28 @@
- 
+#include <virtual_sensor.h>
+
+namespace kinect_fusion {
+
+int main(int argc, char *argv[]) {
+    std::string filenameIn{"../data/rgbd_dataset_freiburg1_xyz/"};
+
+    VirtualSensor sensor;
+    sensor.init(filenameIn)
+
+    FrameData dataFrame{sensor.getDepthIntrinsics(), sensor.getHeight(), sensor.getWidth()};
+
+    sensor.processNextFrame();
+    Map2Df depth = sensor.getDepth();
+    dataFrame.updateValues(depth);
+
+    Surface& previousSurface = dataFrame.getSurface();
+
+    for (unsigned int i = 1; i < sensor.getCurrentFrameCnt(); i++) {
+        sensor.processNextFrame();
+
+        Map2Df depth = sensor.getDepth();
+        dataFrame.updateValues(depth);
+
+        Surface& nextSurface = dataFrame.getSurface();
+    }
+}
+} // namespace kinect_fusion
