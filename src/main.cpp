ebaca7735cb102cf485facb0a7125d1530ab0af6
yumadara <joechiyu@gmail.com>
2022-01-12 22:21:12 +0100

    update again

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <virtual_sensor.h>
-
+#include "utils/include/type_definitions.h"
+#include <PoseEstimator.h>
+#include <data_frame.h>
 namespace kinect_fusion {
 
 int main(int argc, char *argv[]) {
@@ -8,21 +10,29 @@ int main(int argc, char *argv[]) {
     VirtualSensor sensor;
     sensor.init(filenameIn)
 
-    FrameData dataFrame{sensor.getDepthIntrinsics(), sensor.getHeight(), sensor.getWidth()};
+    FrameData previous_dataFrame{sensor.getDepthIntrinsics(), sensor.getHeight(), sensor.getWidth()};
 
     sensor.processNextFrame();
     Map2Df depth = sensor.getDepth();
     dataFrame.updateValues(depth);
 
     Surface& previousSurface = dataFrame.getSurface();
-
+    MatrixXf& pose_estimation = MatrixXf::Identity();
     for (unsigned int i = 1; i < sensor.getCurrentFrameCnt(); i++) {
         sensor.processNextFrame();
-
+        FrameData current_dataFrame{ sensor.getDepthIntrinsics(), sensor.getHeight(), sensor.getWidth() };
         Map2Df depth = sensor.getDepth();
         dataFrame.updateValues(depth);
 
         Surface& nextSurface = dataFrame.getSurface();
+
+        PoseEstimator pose_estimator(current_dataFrame, previous_dataFrame, pose_estimation);
+        
+        for (Level level:  LEVELS)
+        {
+            frame2frameEstimation(Eigen::MatrixXf inputTransformationMatrix, level);
+        }
+        pose_estimation = pose_estimator.getCurrentTransformation();
     }
 }
 } // namespace kinect_fusion
